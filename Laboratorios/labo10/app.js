7992554b46cce5c59eef739bcc03614f2726856b
FerVH26 <00179118@uca.edu.sv>
2019-11-10 20:50:43 -0600

    Add:labo 10

--- Laboratorios/labo10/app.js
+++ Laboratorios/labo10/app.js
@@ -2,14 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var database =require('./bin/database')
+let cors = require('cors');
+var config = require('./config/mongodb');
 
-database.connectDB();
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var registerRouter = require('./routes/register');
 
 var app = express();
+
+config.connect();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,7 +17,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(cors());
+
+app.use('/register', registerRouter);
 
 module.exports = app;
